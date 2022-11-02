2bb3b941e27a6589f68393f4b5c5266a36970c69
Andrés Achury <achuryjaramillo@gmail.com>
2022-11-02 16:23:48 -0500

    chore: Making secure the url of giphy https

--- 04-gift-expert-app/src/helpers/getGifs.js
+++ 04-gift-expert-app/src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=IcS2ANCPXfNGkrse30cQ04PuRf4yjPKw&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=IcS2ANCPXfNGkrse30cQ04PuRf4yjPKw&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
