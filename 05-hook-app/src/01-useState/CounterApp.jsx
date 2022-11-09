6f45338f0c39cbd8ca03e60a0297521d501a99df
Andrés Achury <achuryjaramillo@gmail.com>
2022-11-09 15:53:27 -0500

    useState Hook example with a counter

---
+++ 05-hook-app/src/01-useState/CounterApp.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const CounterApp = () => {
+  const [{ counter1, counter3, counter2 }, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+  return (
+    <>
+      <h1>Counter: {counter1}</h1>
+      <h1>Counter: {counter2}</h1>
+      <h1>Counter: {counter3}</h1>
+
+      <hr />
+
+      <button
+        className="btn"
+        onClick={() =>
+          setCounter((preValue) => ({
+            ...preValue,
+            counter1: preValue.counter1 + 1,
+          }))
+        }
+      >
+        +1
+      </button>
+    </>
+  );
+};
