68d956290f4b162030c4cd7db87428ca3f24b889
Andrés Achury <achuryjaramillo@gmail.com>
2022-11-10 15:41:17 -0500

    chore: example with addEventLister and removeEventListener with an useEffect

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -1,10 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Message = () => {
+  const [coord, setCoord] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
+    const onMouseEvent = ({ x, y }) => {
+      setCoord({ x, y });
+    };
+
+    window.addEventListener("mousemove", onMouseEvent);
     console.log("Message mounted");
 
     return () => {
+      window.removeEventListener("mousemove", onMouseEvent);
       console.log("Message unmounted");
     };
   }, []);
@@ -12,6 +20,7 @@ export const Message = () => {
   return (
     <>
       <h3>Usuario ya existe</h3>
+      <p>{JSON.stringify(coord)}</p>
     </>
   );
 };
