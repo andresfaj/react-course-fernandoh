abe1ed374de910eb1d696308d82884be48998c90
Andrés Achury <achuryjaramillo@gmail.com>
2022-11-13 23:51:23 -0500

    Task finished about memo and useCallback

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CallbackHook } from "./06-memos/CallbackHook";
+// import { CallbackHook } from "./06-memos/CallbackHook";
+import { Padre } from "./07-tarea-memo/Padre";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { Layout } from "./05-useLayoutEffect/Layout";
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <FocusScreen /> */}
     {/* <Memorize /> */}
     {/* <MemoHook /> */}
-    <CallbackHook />
+    {/* <CallbackHook /> */}
+    <Padre />
   </React.StrictMode>
 );
