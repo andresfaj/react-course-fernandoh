15442c94a04d0de84cfcf615bf2bb41c0771d708
Andrés Achury <achuryjaramillo@gmail.com>
2022-12-20 14:52:41 -0500

    test: unit testing for Navbar test component

--- 07-heroes-spa/test/ui/components/navbar/Navbar.test.jsx
+++ 07-heroes-spa/test/ui/components/navbar/Navbar.test.jsx
@@ -1,7 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, useNavigate } from "react-router-dom";
+
 import { AuthContext } from "../../../../src/auth";
 import { Navbar } from "../../../../src/ui";
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUseNavigate,
+}));
 
 describe("Tests on <NavBar /> Component", () => {
   const contextValue = {
@@ -12,7 +20,7 @@ describe("Tests on <NavBar /> Component", () => {
     logout: jest.fn(),
   };
 
-  beforeEach(() => jest.clearAllMocks()); // es bueno cuando se usan hest functions
+  beforeEach(() => jest.clearAllMocks()); // es bueno cuando se usan jest functions
 
   test("should show the users name on the navbar", () => {
     render(
@@ -24,5 +32,21 @@ describe("Tests on <NavBar /> Component", () => {
     );
 
     expect(screen.getByText("Juan Carlos")).toBeTruthy();
+  });
+
+  test("should show the logout and navigate function, when the user pressed the logout btn", () => {
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    const logoutBtn = screen.getByRole("button");
+    fireEvent.click(logoutBtn);
+
+    expect(contextValue.logout).toHaveBeenCalled();
+    expect(mockedUseNavigate).toHaveBeenCalledWith("/login", { replace: true });
   });
 });
