01a523e21c1c07fa32461f3a5aa3064908b3f406
Andrés Achury <achuryjaramillo@gmail.com>
2023-01-02 17:06:54 -0500

    styles: View emptyState when any note it's selected

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,17 +1,11 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoThingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography variant="h1">
-        Qui laboris sit veniam reprehenderit irure officia. Officia nulla duis
-        ea nulla ex ipsum. Lorem excepteur esse excepteur aute laboris amet sunt
-        nostrud voluptate et irure cillum qui ullamco. Fugiat dolor laborum
-        dolor irure. Voluptate ea cillum adipisicing fugiat quis consequat do
-        exercitation officia. In culpa dolor cillum elit occaecat fugiat ipsum
-        ullamco do incididunt sint.
-      </Typography>
+      <NoThingSelectedView />
     </JournalLayout>
   );
 };
